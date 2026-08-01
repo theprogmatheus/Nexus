609e1c4f70bdbcba13bc672eae090137742f652f
theprog.matheus <theprog.matheus@gmail.com>
2026-08-01 13:45:09 -0300

    Adicionar perfis de configuração com variáveis de ambiente

--- src/main/java/io/github/theprogmatheus/web/nexus/NexusApplication.java
+++ src/main/java/io/github/theprogmatheus/web/nexus/NexusApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NexusApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NexusApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NexusApplication.class, args);
+    }
 
 }
